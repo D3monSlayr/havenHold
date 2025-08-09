cca062cd7c8a5ab35bc9ffc9924580e0a4e15f1e
Ferzic <dmonslayr286@gmail.com>
2025-08-09 04:06:57 -0700

    Update BladeOfDawnItem.java

--- src/main/java/me/ferzic/havenHold/Artifacts/BladeOfDawn/BladeOfDawnItem.java
+++ src/main/java/me/ferzic/havenHold/Artifacts/BladeOfDawn/BladeOfDawnItem.java
@@ -1,28 +1,32 @@
 package me.ferzic.havenHold.Artifacts.BladeOfDawn;
 
+import me.ferzic.havenHold.HavenHold;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import java.awt.*;
 import java.util.List;
 
 public class BladeOfDawnItem extends ItemStack {
 
     public BladeOfDawnItem() {
-        // Set base material
         super(Material.DIAMOND_SWORD);
 
         ItemMeta meta = this.getItemMeta();
 
         if (meta != null) {
             meta.lore(List.of(
-                        Component.text("")
-                    )
-            );
-        }
+                    Component.text("A weapon powerful in the daylight..."),
+                    Component.text("Blind your enemies in 5 hits,")
+            ));
 
+            meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+            meta.setUnbreakable(true);
+            this.setItemMeta(meta);
+        } else {
+            HavenHold.getPluginLogger().warning("[BladeOfDawnItem] Meta is null");
+        }
     }
 }
-
